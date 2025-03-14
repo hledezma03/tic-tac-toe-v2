52ae8e5ed7a648ac1f2fe76be8e14911e2c6ce19
hledezma03 <hledezma910@gmail.com>
2025-03-14 10:29:50 -0400

    Create GameController function

--- script.js
+++ script.js
@@ -9,10 +9,58 @@ const Gameboard = (function () {
 
   const makeMove = (cell, player) => {
     if (board[cell] == '0') {
-        board[cell] = player.marker
+        board[cell] = player
       }
   }
 
 
   return {getBoard, makeMove};
 })();
+
+const GameController = (function(
+  playerOneName = "Player One",
+  playerTwoName = "Player Two",
+) {
+  const board = Gameboard;
+
+  const players = [
+    {
+      name: playerOneName,
+      marker: 'X'
+    },
+    {
+      name: playerTwoName,
+      marker: 'O'
+    }
+  ];
+
+  let activePlayer = players[0];
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+  const getActivePlayer = () => activePlayer;
+
+  const printNewRound = () => {
+    board.getBoard();
+
+    console.log(`${getActivePlayer().name}'s turn.`);
+  };
+
+  const playRound = (cell) => {
+    console.log(`${getActivePlayer().name} marked the cell ${cell}...`);
+    board.makeMove(cell, getActivePlayer().marker);
+  
+    /*  This is where we would check for a winner and handle that logic,
+        such as a win message. */
+
+    switchPlayerTurn();
+    printNewRound();
+  };
+
+  printNewRound();
+
+  return {playRound};
+})();
+
+const game = GameController;
